b3146eca8a3e688995e8ca553b22fc6fa753cba2
woodkellymoise5-a11y <woodkellymoise5@gmail.com>
2026-02-06 20:57:01 -0500

    Update author and owner details in settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
-  author: '‎',
+  author: 'Bot‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'BOT.MD', // Your name
+  ownerNumber: '50947398320', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
